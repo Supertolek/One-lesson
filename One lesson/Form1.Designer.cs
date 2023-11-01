dca6d27094c64f09dbf970a67ba5f7ebaf105a57
Et-si-on-debattait <etsiondebattait@gmail.com>
2023-11-01 11:36:21 +0100

    Ajoutez des fichiers projet.

---
+++ One lesson/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace One_lesson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(764, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(404, 322);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 646);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "é";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ One lesson/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace One_lesson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateButtons();
+        }
+
+        private void GenerateButtons()
+        {
+            List<Button> buttons = new List<Button>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int buttonIndex = i;
+                Button button = new Button
+                {
+                    Location = new System.Drawing.Point(20 + i * 235, 20),
+                    Size = new System.Drawing.Size(244, 31),
+                    FlatStyle = FlatStyle.Flat,
+                    TabIndex = i,
+                    BackColor = Color.Transparent // Make the button background transparent
+                };
+                button.FlatAppearance.BorderSize = 0;
+
+                // Create a PictureBox to display the SVG image
+                PictureBox pictureBox = new PictureBox
+                {
+                    Location = new System.Drawing.Point(0, 0),
+                    Size = new Size(244, 31),
+                    BackColor = Color.Transparent // Make the PictureBox background transparent
+                };
+
+                // Load the SVG image into the PictureBox (Replace "your_svg_image.svg" with your SVG file)
+                pictureBox.Load("Resources/tab.png");
+
+                // Add the PictureBox to the button
+                button.Controls.Add(pictureBox);
+
+                // Add the event handler using the captured buttonIndex
+                button.Click += (sender, e) =>
+                {
+                    MessageBox.Show("Button " + (buttonIndex + 1) + " clicked");
+                };
+                pictureBox.Click += (sender, e) =>
+                {
+                    MessageBox.Show("Button " + (buttonIndex + 1) + " clicked");
+                };
+
+                buttons.Add(button);
+                Controls.Add(button);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
